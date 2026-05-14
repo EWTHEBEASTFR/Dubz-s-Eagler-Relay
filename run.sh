b75e763897786953e0f0f05f7d4a89ae5c4d667e
EWTHEBEASTFR <132291550+EWTHEBEASTFR@users.noreply.github.com>
2026-05-14 09:15:47 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 echo ""
-echo "Eaglercraft 1.5.2 LAN World Relay"
-echo "Note: Requires java 8 or newer!"
-while true
-	do
-		echo ""
-		java -Xmx1024M -Xms1024M -jar EaglerSPRelay.jar
-		echo ""
-		echo "press [CTRL+C] to cancel restart"
-	sleep 5
-done
+echo "Starting Eaglercraft Relay..."
+echo ""
+
+java -Xmx512M -jar EaglerSPRelay.jar
